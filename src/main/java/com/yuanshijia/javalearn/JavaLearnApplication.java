03343d91abda653850a93fb15ddad889064040a1
yuan491187718@gmail.com <yuan491187718@gmail.com>
2019-05-22 20:15:17 +0800

    Initial commit

---
+++ src/main/java/com/yuanshijia/javalearn/JavaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.yuanshijia.javalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLearnApplication.class, args);
+    }
+
+}
