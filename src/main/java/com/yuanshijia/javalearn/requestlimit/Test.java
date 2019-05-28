7fc787a823b2a5dcd0a329d2595847b4159b940b
yuan491187718@gmail.com <yuan491187718@gmail.com>
2019-05-28 17:01:24 +0800

    限流算法-令牌桶限流

---
+++ src/main/java/com/yuanshijia/javalearn/requestlimit/Test.java
@@ -0,0 +1,18 @@
+package com.yuanshijia.javalearn.requestlimit;
+
+/**
+ * @author yuan
+ * @date 2019/5/28
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        TokenBucket tokenBucket = new TokenBucket(100, 1);
+        for (int i = 0; i < 200; i++) {
+            if (i == 10 || i == 180) {
+                Thread.sleep(500);
+            }
+            System.out.println("第" + i + "次请求结果=" + tokenBucket.getToken());
+        }
+    }
+}
--- src/main/java/com/yuanshijia/javalearn/requestlimit/TokenBucket.java
+++ src/main/java/com/yuanshijia/javalearn/requestlimit/TokenBucket.java
@@ -84,13 +84,4 @@ public class TokenBucket {
         return true;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        TokenBucket tokenBucket = new TokenBucket(100, 1);
-        for (int i = 0; i < 200; i++) {
-            if (i == 10 || i == 180) {
-                Thread.sleep(500);
-            }
-            System.out.println("第" + i + "次请求结果=" + tokenBucket.getToken());
-        }
-    }
 }
